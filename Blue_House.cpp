77796aa33f450c3fbc0899d6db46200b32063408
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-10 15:46:07 -0500

    Casita_Azul
    Cree la funcion Inicial mediante un char Genero una lista de claves y otra de usuarios Predefino La variable de usuaripo y de contraseña

---
+++ Blue_House.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Declarar e inicializar una lista de números enteros
+    Char Preset_Usuerser[5] = {"Daniel","Juan","Danna",};
+    int Preset_Key[5] = {34523,12321,12345};
+    
+    // Valor a buscar, ingresado por el usuario
+    int User;
+	int Key;
+	
+    // Solicitar al usuario que ingrese el valor a buscar
+    printf("Ingresa el Usuario: ");
+    scanf("%d", &valor_a_buscar);
