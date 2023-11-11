6f19dea99de07c73be38f79ed12bf9b57018560c
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-11 14:45:38 -0500

    Blue_House 1.0.3
    Se solucionaron varios bugs relacionados con el como se guardaban las variables con lo que si se encuentra una conicidencia cambia la variable Verificado a 1

--- Blue_House.cpp
+++ Blue_House.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     // Declarar e inicializar una lista de números enteros
-    Char Preset_Usuerser[3][20] = {"Daniel","Juan","Danna",};
-    int Preset_Key[3] = {34523,12321,12345};
+    char Preset_User[3][20] = {"Daniel","Juan","Danna",};
+    int Preset_Key[3] = {34523, 12321, 12345};
     
     // Valor a buscar, ingresado por el usuario
     char User[20];
@@ -11,12 +12,14 @@ int main() {
 	
     // Solicitar al usuario que ingrese el valor a buscar
     printf("Ingresa el Usuario: ");
-    scanf("%d", &User_Search);
+    scanf("%s", User);
     
     printf("Ingresa la clave: ");
-    scanf("%d", &Key_Search);
+    scanf("%d", &Key);
     
     int Verificado = 0;
     
-    int encontrado = 0;
     for (int i = 0; i < 3; i++) {
+    	if (strcmp(User, Preset_User[i]) == 0 && Key, Preset_Key[i]){
+    		
+		}
