e3fc4352011a440b6ce92f79a2b74bce7f629331
Danielpveran <141794710+Danielpveran@users.noreply.github.com>
2023-11-11 13:47:30 -0500

    Blue_House 1.0.2
    Genero una Variable de verificado y el for para revisar el char donde se guardan las claves y usuarios

--- Blue_House.cpp
+++ Blue_House.cpp
@@ -2,11 +2,11 @@
 
 int main() {
     // Declarar e inicializar una lista de números enteros
-    Char Preset_Usuerser[3] = {"Daniel","Juan","Danna",};
+    Char Preset_Usuerser[3][20] = {"Daniel","Juan","Danna",};
     int Preset_Key[3] = {34523,12321,12345};
     
     // Valor a buscar, ingresado por el usuario
-    int User;
+    char User[20];
 	int Key;
 	
     // Solicitar al usuario que ingrese el valor a buscar
@@ -15,3 +15,8 @@ int main() {
     
     printf("Ingresa la clave: ");
     scanf("%d", &Key_Search);
+    
+    int Verificado = 0;
+    
+    int encontrado = 0;
+    for (int i = 0; i < 3; i++) {
